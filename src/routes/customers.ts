d31bfd422195c82a516f481240ea9190b87ce9a5
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-03-18 12:53:13 +0530

    Add getCustomers

--- src/routes/customers.ts
+++ src/routes/customers.ts
@@ -1,10 +1,12 @@
 import { createCustomerController } from "../controllers/createCustomer";
+import { getCustomersController } from "../controllers/getCustomers"
 
 const express  = require('express');
 const router = express.Router()
 
 console.log('customers route')
 
+router.get('/', getCustomersController)
 router.post('/', createCustomerController)
 
 module.exports = router
